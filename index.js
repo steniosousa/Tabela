89b8fa04bada0eaa495a3e5cfbdfbd3273572238
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2021-10-12 12:22:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-let copiarTexto = () =>{
-    //captura o elemento input
-    const inputTest = document.querySelector("#inputTest");
-    
-    //seleciona todo o texto do elemento
-    inputTest.select();
-    //executa o comando copy
-    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
-    document.execCommand('copy');
-};
+
